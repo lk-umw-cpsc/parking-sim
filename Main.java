1a7164357c90523496c45fb7fda146e2ce7f07f5
Lauren Knight <lknight2@mail.umw.edu>
2022-07-07 13:21:56 -0400

    comments

--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
 import javax.swing.SwingUtilities;
 
 /**
- * Program entry point
+ * Program entry point class.
  * 
  * @author Lauren Knight
  */
 public class Main {
 
+    /**
+     * Program entry point
+     * @param args Arguments passed to the program by the OS
+     */
     public static void main(String[] args) {
         // initialize the GUI elements on the Swing event thread (required)
-        SwingUtilities.invokeLater(Main::initiateSwingComponents);
+        SwingUtilities.invokeLater(Main::initializeSwingComponents);
     }
 
-    private static void initiateSwingComponents() {
+    /**
+     * This method will be called on the Swing UI event thread.
+     * 
+     * Creates the main frame for the application and all of its
+     * children, causing the program to start.
+     */
+    private static void initializeSwingComponents() {
         // create a new ApplicationFrame (our main window) and make it visible
         ApplicationFrame frame = new ApplicationFrame();
         frame.setLocationRelativeTo(null);
